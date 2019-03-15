b64967031822998e7a67b83772092353bb91e752
Emulatory <32264365+Emulatory@users.noreply.github.com>
2019-03-15 11:29:13 +0000

    Collisions implemented, class can't be found?

--- keyboard.py
+++ keyboard.py
@@ -12,21 +12,21 @@ class Keyboard:
         self.up = False
 
     def keyDown(self, key):
-        if key == simplegui.KEY_MAP['right']:
+        if key == simplegui.KEY_MAP['d']:
             self.right = True
-        if key == simplegui.KEY_MAP['left']:
+        if key == simplegui.KEY_MAP['a']:
             self.left = True
-        if key == simplegui.KEY_MAP['up']:
+        if key == simplegui.KEY_MAP['w']:
             self.up = True
-        if key == simplegui.KEY_MAP['down']:
+        if key == simplegui.KEY_MAP['s']:
             self.down = True
 
     def keyUp(self, key):
-        if key == simplegui.KEY_MAP['right']:
+        if key == simplegui.KEY_MAP['d']:
             self.right = False
-        if key == simplegui.KEY_MAP['left']:
+        if key == simplegui.KEY_MAP['a']:
             self.left = False
-        if key == simplegui.KEY_MAP['up']:
+        if key == simplegui.KEY_MAP['w']:
             self.up = False
-        if key == simplegui.KEY_MAP['down']:
+        if key == simplegui.KEY_MAP['s']:
             self.down = False
