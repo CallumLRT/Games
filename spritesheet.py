6b6c3665ce657d0a6767699fd29ecb6ba4368ae7
Zeta616 <m.matczak@yahoo.co.uk>
2019-03-21 23:37:35 +0000

    added an optional delay between frames

--- spritesheet.py
+++ spritesheet.py
@@ -6,7 +6,7 @@ except ImportError:
 
 # Don't touch this anymore
 class SpriteSheet:
-    def __init__(self, url, maxIndex):
+    def __init__(self, url, maxIndex, frameDelay=1):
         self.url = url
         self.img = simplegui.load_image(url)
         self.index = (0, 0)
@@ -15,6 +15,8 @@ class SpriteSheet:
         self.frameHeight = self.img.get_height() / maxIndex[1]
         self.frameCentreX = self.frameWidth / 2
         self.frameCentreY = self.frameHeight / 2
+        self.frameDelay = frameDelay
+        self.frame = 0
 
     def draw(self, canvas, pos):
         canvas.draw_image(self.img,
@@ -26,8 +28,11 @@ class SpriteSheet:
                           )
 
     def update(self):
-        self.index = (self.index[0] + 1, self.index[1])
-        if self.index[0] >= self.maxIndex[0]:
-            self.index = (0, self.index[1] + 1)
-        if self.index[1] >= self.maxIndex[1]:
-            self.index = (0, 0)
+        self.frame += 1
+        if self.frame == self.frameDelay:
+            self.index = (self.index[0] + 1, self.index[1])
+            if self.index[0] >= self.maxIndex[0]:
+                self.index = (0, self.index[1] + 1)
+            if self.index[1] >= self.maxIndex[1]:
+                self.index = (0, 0)
+            self.frame = 0
