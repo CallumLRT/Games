19761f9841280ae2d0f26142832602bf935ba3ca
Zeta616 <m.matczak@yahoo.co.uk>
2019-03-21 23:44:46 +0000

    change animation for direction walking in

--- playerInteraction.py
+++ playerInteraction.py
@@ -14,9 +14,16 @@ class PlayerInteraction:
     def update(self):
         if self.keyboard.right:
             self.wheel.vel.add(Vector(1, 0))
+            self.wheel.PlayerSprite_current = self.wheel.PlayerSprite_right
+
         if self.keyboard.left:
             self.wheel.vel.add(Vector(-1, 0))
+            self.wheel.PlayerSprite_current = self.wheel.PlayerSprite_left
+
         if self.keyboard.down:
             self.wheel.vel.add(Vector(0, 1))
+            self.wheel.PlayerSprite_current = self.wheel.PlayerSprite_down
+
         if self.keyboard.up:
             self.wheel.vel.add(Vector(0, -1))
+            self.wheel.PlayerSprite_current = self.wheel.PlayerSprite_up
